85826ed8fb4a2f82ed8c6afafd44672f08c233d0
Aniket Fadia <aniketf@uw.edu>
2023-07-10 12:24:17 -0700

    Add Download Feature

--- src/app/main.py
+++ src/app/main.py
@@ -8,15 +8,17 @@ things end to end and then work towards improving individual modules.
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode
-
+from app.frontend import components
 from genetic_testing.routers import ncbi
+from typing import List
 
 if "SUMMARY_FORM_SUBMIT" not in st.session_state:
     st.session_state["SUMMARY_FORM_SUBMIT"] = False
+    st.session_state["FILTER"] = False
 
 if "df_summary" not in st.session_state:
     st.session_state["df_summary"] = pd.DataFrame()
-    st.session_state["rerun"] = False
+    # st.session_state["rerun"] = False
     st.session_state["summary_form_submit_count"] = 0
 
 # AgGrid
@@ -25,19 +27,6 @@ defaultColDef = {
     "resizable": True,
     "sortable": True,
     "editable": True,   
-}
-
-# column_defs = [{"headerName": "Title", 
-#                 "field": "Title", 
-#                 "filter": "agMultiColumnFilter",
-#                 "filterParams": {
-#                     "filterOptions": ["contains", "notContains"]
-#                 }}]
-
-options = {
-    "rowSelection": "multiple",
-    "rowMultiSelectWithClick": True,
-    "enableRangeSelection": True,
 }
 
 def rerun():
@@ -54,12 +43,17 @@ def get_data(database, search_term):
 def preprocess_summary_data():
     # Extract the species
     st.session_state["df_summary"]["Species"] = st.session_state["df_summary"]["Title"].apply(lambda x: ' '.join(x.split()[:2]))
+    # Convert the species to categorical
+    st.session_state["df_summary"]["Species"] = st.session_state["df_summary"]["Species"].astype('category')
 
     # Reorder the columns
     column_order = ["Species", "Title", "TaxId", "Id", "Length", "Gi", "CreateDate", "UpdateDate", "Status"]
     st.session_state["df_summary"] = st.session_state["df_summary"].reindex(columns=column_order)
 
 st.title("Neglected Diagnostics: Perform Genetic Testing At Scale!")
+
+def get_download_ids(df: pd.DataFrame) -> List[int]:
+    return df["Id"].unique()
 
 with st.form("query"):    
     database = st.selectbox("Select the database to search", ("nucleotide", "gene"))
@@ -76,25 +70,25 @@ with st.form("query"):
         
         st.session_state["SUMMARY_FORM_SUBMIT"] = True
         
-selected_df = pd.DataFrame()
+df_aggrid_output = pd.DataFrame()
 if st.session_state["SUMMARY_FORM_SUBMIT"]:
-    #st.write("Number of rows in df_summary_edited before: ", len(st.session_state["df_summary"]))
-    
+    #st.write("Number of rows in df_summary_edited before: ", len(st.session_state["df_summary"]))    
     #st.session_state["df_summary"] = st.data_editor(st.session_state["df_summary"], num_rows="dynamic", on_change=rerun)
     
     options_builder = GridOptionsBuilder.from_dataframe(st.session_state["df_summary"])
     options_builder.configure_pagination(paginationAutoPageSize=False, paginationPageSize=10)
     options_builder.configure_side_bar()
-    options_builder.configure_selection("multiple", use_checkbox=True)
+    #options_builder.configure_selection("multiple", use_checkbox=True)
     options_builder.configure_column("Title", filter="agMultiColumnFilter", filterParams={"maxNumConditions": 4})
     options_builder.configure_column("Species", filter="agMultiColumnFilter")
     options_builder.configure_default_column(**defaultColDef)
-    options_builder.configure_grid_options(**options)
+    #options_builder.configure_grid_options(**options)
     grid_options = options_builder.build()
     grid_table = AgGrid(st.session_state["df_summary"], 
                         gridOptions=grid_options,
                         update_mode=GridUpdateMode.MODEL_CHANGED,
-                        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,)
+                        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+                        enable_enterprise_modules=False)
     
     #st.write("Number of rows in df_summary_edited after: ", len(grid_table["data"]))
     #st.write(grid_table["data"])
@@ -105,9 +99,18 @@ if st.session_state["SUMMARY_FORM_SUBMIT"]:
     #     st.write('Selected rows')
     #     st.dataframe(selected_df)
 
-    filtered_df = grid_table['data']
-    st.write('Filtered rows')
-    st.dataframe(filtered_df)
+    df_aggrid_output = grid_table['data']
+    # st.write('Filtered rows')
+    # st.dataframe(filtered_df)
+    
+df_filter_output = components.filter_dataframe(df_aggrid_output)
+if st.session_state["FILTER"]:
+    st.dataframe(df_filter_output)
+    st.write("Size of final dataframe: ", len(df_filter_output))
+
+    if st.button('Download'):
+        document = ncbi.fetch(database, get_download_ids(df_filter_output))
+        st.write(document)
     
 
 #st.write(st.session_state)
@@ -116,15 +119,7 @@ if st.session_state["SUMMARY_FORM_SUBMIT"]:
 #     st.session_state.rerun = False
 #     st.experimental_rerun()
 
-
-
-
 # download_ids = st.text_input(
 #     label="Enter the id of the species to download the sequence data",
 #     placeholder="2155465881")
-
-# if st.button('Download'):
-#     document = ncbi.fetch(database, download_ids)
-#     st.write(document)
-
 
--- src/genetic_testing/routers/ncbi.py
+++ src/genetic_testing/routers/ncbi.py
@@ -5,25 +5,39 @@ functionality
 """
 
 import os
+
+outdir = "ncbi_downloads"
+os.makedirs(outdir, exist_ok=True)
 from typing import Dict, List, Union
 
 import streamlit as st
+from Bio import SeqIO
 from Bio import Entrez
 from Bio.Entrez.Parser import DictionaryElement, ListElement
 
-ENTREZ_EMAIL = 'ENTREZ_EMAIL'
-SUMMARY_FIELDS = ['Id', 'Title', 'Gi', 'CreateDate', 'UpdateDate', 'TaxId', 'Length', 'Status']
+ENTREZ_EMAIL = "ENTREZ_EMAIL"
+SUMMARY_FIELDS = [
+    "Id",
+    "Title",
+    "Gi",
+    "CreateDate",
+    "UpdateDate",
+    "TaxId",
+    "Length",
+    "Status",
+]
 
 if ENTREZ_EMAIL in st.secrets:
     Entrez.email = st.secrets[ENTREZ_EMAIL]
 else:
-    Entrez.email = os.environ.get(ENTREZ_EMAIL, '')
+    Entrez.email = os.environ.get(ENTREZ_EMAIL, "")
+
 
 def search(database: str, term: str) -> List[int]:
     """Get the List of UIDs matching the Entrez query.
 
-    This function searches the provided NCBI database and returns the list of UIDs matching the 
-    Entrez query.  
+    This function searches the provided NCBI database and returns the list of UIDs matching the
+    Entrez query.
 
     Args:
         database (str): Name of the Entrez database.
@@ -36,19 +50,20 @@ def search(database: str, term: str) -> List[int]:
     uids = []
     if handle:
         record = Entrez.read(handle)
-        uids = record['IdList']
+        uids = record["IdList"]
 
     return uids
 
+
 def summary(database: str, ids: Union[int, List[int]]) -> DictionaryElement:
     """Get the document summaries for the input UIDs.
-    
+
     This function returns the document summaries for the input list of UIDs and datasource.
-    
+
     Args:
         database (str): Name of the Entrez database.
         term (str): Entrez text query.
-    
+
     Returns:
         : A list of UIDs.
     """
@@ -58,30 +73,40 @@ def summary(database: str, ids: Union[int, List[int]]) -> DictionaryElement:
         handle.close()
     return record
 
+
 def parse_summary(document_summaries: ListElement) -> List[Dict[str, str]]:
     """Get the document summaries for the input UIDs.
-    
+
     This function returns the document summaries for the input list of UIDs and datasource.
-    
+
     Args:
-    
+
     Returns:
     """
     parsed_summaries = []
     for document_summary in document_summaries:
-        parsed_summaries.append({key: document_summary.get(key, None) for key in SUMMARY_FIELDS})
+        parsed_summaries.append(
+            {key: document_summary.get(key, None) for key in SUMMARY_FIELDS}
+        )
     return parsed_summaries
 
-def fetch(database: str, ids: Union[int, List[int]]) -> DictionaryElement:
+
+def fetch(database: str, ids: Union[int, List[int]]) -> str:
     """Download the document summaries for the input UIDs.
-    
+
     This function downloads the document summaries for the input list of UIDs and datasource.
-    
+
     Args:
-    
+
     Returns:
     """
-    handle = Entrez.efetch(db=database, id=ids, retmax=10000, rettype='fasta')
+    handle = Entrez.efetch(db=database, id=ids, retmax=10000, rettype="fasta")
     if handle:
-        record = handle.read()
+        sequence_records = SeqIO.parse(handle, "fasta")
+        records_written = SeqIO.write(
+            sequence_records, os.path.join(outdir, "example.fasta"), "fasta"
+        )
+        print(f"Successfully downloaded {records_written} records")
+        record = "File Downloaded Successfully!"
+        handle.close()
     return record
