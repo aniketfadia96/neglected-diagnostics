cfcb77e46a4ccbc339c12a40fec4f37246fc07d0
Aniket Fadia <aniketf@uw.edu>
2023-07-11 15:49:43 -0700

    Update additional filters

--- src/app/frontend/components.py
+++ src/app/frontend/components.py
@@ -24,13 +24,39 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 
     df = df.copy()
 
+    # print(df.info())
+    # print(df["CreateDate"])
+    
+    # Convert columns to categorical
+    df["Species"] = df["Species"].astype("category")
+    df["TaxId"] = df["TaxId"].astype("category")
+    df["Id"] = df["Id"].astype("category")
+    df["Gi"] = df["Gi"].astype("category")
+    df["Status"] = df["Status"].astype("category")
+
+    # Convert columns to date
+    #df["CreateDate"] = pd.to_datetime(df["CreateDate"], format="%Y/%m/%d")
+    #df["UpdateDate"] = pd.to_datetime(df["UpdateDate"], format="%Y/%m/%d")
+    
+    # Convert StringElement objects to Unicode strings
+    date_strings = [str(date_element) for date_element in df["CreateDate"]]
+
+    # Convert the Unicode strings to datetime format
+    df["CreateDate"] = pd.to_datetime(date_strings, format="%Y/%m/%d")
+
+    # Convert StringElement objects to Unicode strings
+    date_strings = [str(date_element) for date_element in df["UpdateDate"]]
+
+    # Convert the Unicode strings to datetime format
+    df["UpdateDate"] = pd.to_datetime(date_strings, format="%Y/%m/%d")
+
     # Try to convert datetimes into a standard format (datetime, no timezone)
     for col in df.columns:
-        if is_object_dtype(df[col]):
-            try:
-                df[col] = pd.to_datetime(df[col])
-            except Exception:
-                pass
+        # if is_object_dtype(df[col]):
+        #     try:
+        #         df[col] = pd.to_datetime(df[col], format='%Y/%m/%d')
+        #     except Exception:
+        #         pass
 
         if is_datetime64_any_dtype(df[col]):
             df[col] = df[col].dt.tz_localize(None)
@@ -53,15 +79,25 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
             elif is_numeric_dtype(df[column]):
                 _min = float(df[column].min())
                 _max = float(df[column].max())
-                step = (_max - _min) / 100
-                user_num_input = right.slider(
-                    f"Values for {column}",
-                    min_value=_min,
-                    max_value=_max,
-                    value=(_min, _max),
-                    step=step,
-                )
-                df = df[df[column].between(*user_num_input)]
+                # step = (_max - _min) / 100
+                # user_num_input = right.slider(
+                #     f"Values for {column}",
+                #     min_value=_min,
+                #     max_value=_max,
+                #     value=(_min, _max),
+                #     step=step,
+                # )
+                with right:
+                    # Create a row layout
+                    sub_columns = st.columns(2)
+                    # Add input boxes for minimum and maximum values in the row
+                    with sub_columns[0]:
+                        min_length = st.number_input("Enter Minimum Length")
+
+                    with sub_columns[1]:
+                        max_length = st.number_input("Enter Maximum Length")
+
+                df = df[df[column].between(min_length, max_length)]
             elif is_datetime64_any_dtype(df[column]):
                 user_date_input = right.date_input(
                     f"Values for {column}",
--- src/app/main.py
+++ src/app/main.py
@@ -26,94 +26,139 @@ defaultColDef = {
     "filter": True,
     "resizable": True,
     "sortable": True,
-    "editable": True,   
+    "editable": True,
 }
+
 
 def rerun():
     st.session_state.rerun = True
 
+
 def get_data(database, search_term):
     uids = ncbi.search(database, search_term)
-    print('Total number of documents returned for the above search query: ' + str(len(uids)))
+    print(
+        "Total number of documents returned for the above search query: "
+        + str(len(uids))
+    )
     document_summaries = ncbi.summary(database, uids)
     parsed_summaries = ncbi.parse_summary(document_summaries)
     df_summaries = pd.DataFrame.from_dict(parsed_summaries)
     return df_summaries
 
+
 def preprocess_summary_data():
     # Extract the species
-    st.session_state["df_summary"]["Species"] = st.session_state["df_summary"]["Title"].apply(lambda x: ' '.join(x.split()[:2]))
+    st.session_state["df_summary"]["Species"] = st.session_state["df_summary"][
+        "Title"
+    ].apply(lambda x: " ".join(x.split()[:2]))
     # Convert the species to categorical
-    st.session_state["df_summary"]["Species"] = st.session_state["df_summary"]["Species"].astype('category')
+    st.session_state["df_summary"]["Species"] = st.session_state["df_summary"][
+        "Species"
+    ].astype("category")
 
     # Reorder the columns
-    column_order = ["Species", "Title", "TaxId", "Id", "Length", "Gi", "CreateDate", "UpdateDate", "Status"]
-    st.session_state["df_summary"] = st.session_state["df_summary"].reindex(columns=column_order)
+    column_order = [
+        "Species",
+        "Title",
+        "TaxId",
+        "Id",
+        "Length",
+        "Gi",
+        "CreateDate",
+        "UpdateDate",
+        "Status",
+    ]
+    st.session_state["df_summary"] = st.session_state["df_summary"].reindex(
+        columns=column_order
+    )
 
 st.title("Neglected Diagnostics: Perform Genetic Testing At Scale!")
 
 def get_download_ids(df: pd.DataFrame) -> List[int]:
     return df["Id"].unique()
 
-with st.form("query"):    
+
+with st.form("query"):
     database = st.selectbox("Select the database to search", ("nucleotide", "gene"))
-    search_term = st.text_input(label="Enter the search term", placeholder="Example: human[organism] AND topoisomerase[protein name]")
-    
+    search_term = st.text_input(
+        label="Enter the search term",
+        placeholder="Example: human[organism] AND topoisomerase[protein name]",
+    )
+
     submitted = st.form_submit_button("Submit")
     if submitted:
         st.session_state["summary_form_submit_count"] += 1
-        print("Summary form submission count",  st.session_state["summary_form_submit_count"])
-        
+        print(
+            "Summary form submission count",
+            st.session_state["summary_form_submit_count"],
+        )
+
         st.session_state["df_summary"] = get_data(database, search_term)
-        print("Number of rows in summary after fetching data: ", len(st.session_state["df_summary"]))
+        print(
+            "Number of rows in summary after fetching data: ",
+            len(st.session_state["df_summary"]),
+        )
         preprocess_summary_data()
-        
+
         st.session_state["SUMMARY_FORM_SUBMIT"] = True
-        
+
 df_aggrid_output = pd.DataFrame()
 if st.session_state["SUMMARY_FORM_SUBMIT"]:
-    #st.write("Number of rows in df_summary_edited before: ", len(st.session_state["df_summary"]))    
-    #st.session_state["df_summary"] = st.data_editor(st.session_state["df_summary"], num_rows="dynamic", on_change=rerun)
-    
+    # st.write("Number of rows in df_summary_edited before: ", len(st.session_state["df_summary"]))
+    # st.session_state["df_summary"] = st.data_editor(st.session_state["df_summary"], num_rows="dynamic", on_change=rerun)
+
     options_builder = GridOptionsBuilder.from_dataframe(st.session_state["df_summary"])
-    options_builder.configure_pagination(paginationAutoPageSize=False, paginationPageSize=10)
+    options_builder.configure_pagination(
+        paginationAutoPageSize=False, paginationPageSize=10
+    )
     options_builder.configure_side_bar()
-    #options_builder.configure_selection("multiple", use_checkbox=True)
-    options_builder.configure_column("Title", filter="agMultiColumnFilter", filterParams={"maxNumConditions": 4})
+    # options_builder.configure_selection("multiple", use_checkbox=True)
+    options_builder.configure_column(
+        "Title", filter="agMultiColumnFilter", filterParams={"maxNumConditions": 4}
+    )
     options_builder.configure_column("Species", filter="agMultiColumnFilter")
     options_builder.configure_default_column(**defaultColDef)
-    #options_builder.configure_grid_options(**options)
+    # options_builder.configure_grid_options(**options)
     grid_options = options_builder.build()
-    grid_table = AgGrid(st.session_state["df_summary"], 
-                        gridOptions=grid_options,
-                        update_mode=GridUpdateMode.MODEL_CHANGED,
-                        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-                        enable_enterprise_modules=False)
-    
-    #st.write("Number of rows in df_summary_edited after: ", len(grid_table["data"]))
-    #st.write(grid_table["data"])
-    #st.write(type(grid_table))
-    #st.write(grid_table.keys())
-    #selected_df = grid_table["selected_rows"]
+    grid_table = AgGrid(
+        st.session_state["df_summary"],
+        gridOptions=grid_options,
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        data_return_mode=DataReturnMode.FILTERED,
+        enable_enterprise_modules=False,
+    )
+
+    # st.write("Number of rows in df_summary_edited after: ", len(grid_table["data"]))
+    # st.write(grid_table["data"])
+    # st.write(type(grid_table))
+    # st.write(grid_table.keys())
+    # selected_df = grid_table["selected_rows"]
     # if selected_df:
     #     st.write('Selected rows')
     #     st.dataframe(selected_df)
 
-    df_aggrid_output = grid_table['data']
+    df_aggrid_output = grid_table["data"]
     # st.write('Filtered rows')
     # st.dataframe(filtered_df)
-    
+
 df_filter_output = components.filter_dataframe(df_aggrid_output)
 if st.session_state["FILTER"]:
     st.dataframe(df_filter_output)
     st.write("Size of final dataframe: ", len(df_filter_output))
 
-    if st.button('Download'):
+    if st.button("Prepare Download"):
         file_buffer = ncbi.fetch(database, get_download_ids(df_filter_output))
-        st.download_button(label="Download", data=file_buffer.getvalue(), file_name="example.fasta")
-        #st.write(document)
-    
-#st.write(st.session_state)
+
+        if len(file_buffer.getvalue()) == 0:
+            st.write("No data to download. Please refine your filters")
+        else:
+            st.write("Fetched data successfully!")
+            st.download_button(
+                label="Download", data=file_buffer.getvalue(), file_name="data.fasta"
+            )
+        # st.write(document)
+
+# st.write(st.session_state)
 
 # if st.session_state.rerun:
 #     st.session_state.rerun = False
@@ -122,4 +167,3 @@ if st.session_state["FILTER"]:
 # download_ids = st.text_input(
 #     label="Enter the id of the species to download the sequence data",
 #     placeholder="2155465881")
-
